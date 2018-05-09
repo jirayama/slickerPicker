a48c1445499d94b25555e02bcb4caadd6b8af3a3
jirayama <jillianedward@gmail.com>
2018-05-09 13:25:26 +0900

    Update sp.js

--- sp.js
+++ sp.js
@@ -5,6 +5,7 @@ function sp(opt) {
 	var readout = 0,
 			current_rgba = opt.rbga || [],
 			current_a = 1,
+			current_h = 50,
 			shades = {
 				lighter: [],
 				light: [],
@@ -81,14 +82,30 @@ function sp(opt) {
 	module.appendMany(module_top,	module_bottom);
 
 	//easing
-	Math.easeOutCubic = function (t, b, c, d) {
-		return c*((t=t/d-1)*t*t + 1) + b;
-	};
+	Math.easeInQuad = function (t, b, c, d) {
+	t /= d;
+	return c*t*t + b;
+};
+	Math.easeOutQuad = function (t, b, c, d) {
+	t /= d;
+	return -c * t*(t-2) + b;
+};
+Math.easeInOutQuad = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
 
-	Math.easeInOutSine = function (t, b, c, d) {
-		return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
-	};
-
+Math.easeOutExpo = function (t, b, c, d) {
+	return c * ( -Math.pow(2, -10 * t/d ) + 1 ) + b;
+};
+Math.easeOutExpoSuper = function (t, b, c, d) {
+	return c * ( -Math.pow( 10, -3 * t/d ) + 1 ) + b;
+};
+Math.linearTween = function (t, b, c, d) {
+	return c*t/d + b;
+};
 
 
 	// actions
@@ -97,27 +114,37 @@ function sp(opt) {
       var s = 0,
           l = 100,
           endL = 50,
-          startL = l,
-          startS = s,
+          startL = 100,
+          startS = 100,
           endS = 100,
           x = 0,
           y = 0,
-          color;
+          color,
+          rate = 200,          
+          dist = (endS - startS)/200,
+          incr = (endS - startS)/200/130;
 
       colorPicker_ctx.clearRect(0, 0, 200, 130);
 
       for (y = 0; y <= 130; y++) {
-        for (x = 0; x <= 200; x++) {
-          color = hsl2rbg(h, s, l);
-          colorPicker_ctx.fillStyle = "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
-          colorPicker_ctx.fillRect(x, y, 1, 1);
-          s = Math.easeInOutSine(x, 0, 1, 200)*100;
-          l = Math.easeOutCubic(x, startL, -(startL - endL), 200);
+        for (x = 0; x <= 200; x++) {       
+          s = Math.easeInQuad(x, startS, (100 - startS ), 200);
+          l = Math.easeOutQuad(x, startL, -(startL - endL), 200); 
+          colorPicker_ctx.fillStyle = "hsl(" + h + "," + s + "%," + l + "%)";
+          // if(l>25 && l<26 || l>50 && l<51 || l>75 && l<76) {
+          // 	colorPicker_ctx.fillStyle = "white";
+          // } 
+          // if(s>25 && s < 26 || s>50 && s < 51 ||s>75 && s < 76 || s>99){
+          // 	colorPicker_ctx.fillStyle = "red";
+          // }
+          // if( s < 1){
+          // 	colorPicker_ctx.fillStyle = "yellow";
+          // }
+          colorPicker_ctx.fillRect(x, y, 1, 1);             
         }
-        s = 0;   
-        endL = Math.easeInOutSine(y, endL, -1, 130);
-        startL = Math.easeOutCubic(y, startL, -1, 130);
-        l = startL;
+      	startS = Math.easeOutExpo(y, 100, -100, 130);
+      	startL = Math.linearTween(y, 100, -100, 130);
+      	endL = Math.easeInQuad(y, 50, -50, 130); 
       }
     }
 	function draw_huePicker() {
@@ -157,12 +184,42 @@ function sp(opt) {
 
 	// init 
 	update_readout_display();
-	draw_colorPicker(320);
+	draw_colorPicker(current_h);
 	draw_huePicker();
+
+	module.addEventListener('wheel', function(e){
+		if(e.deltaY === -100 ){
+			current_h = current_h  - 10;
+			console.log("-", current_h)
+		} 
+		if(e.deltaY === 100 ){
+			current_h = current_h  + 10;
+			console.log("+", current_h)
+		}
+
+		if(current_h<=0){
+			current_h = 1;
+		}
+		if(current_h>360){
+			current_h = 360;
+		}
+		draw_colorPicker(current_h);
+	})
+
+	function loop(){
+		current_h++;
+		if(current_h>360){
+			current_h = 1;
+		}
+		draw_colorPicker(current_h);
+		requestAnimationFrame(loop);
+	}
 
 	// test output
 	document.getElementById('test1').appendMany(module);
 	console.log(module);
+
+	loop();
 }
 
 
