5cc5022e2cf02339515dbe07175b3060e6d234b1
jirayama <jillianedward@gmail.com>
2018-05-12 17:03:49 +0900

    Update sp.js

--- sp.js
+++ sp.js
@@ -2,13 +2,16 @@ function sp(opt) {
 	// state
 	opt = opt || {};
 
-	var readout = 0,			
+	var readout = 0,
 			current_a = 1,
 			current_h = 14,
 			current_s = 100,
 			current_l = 50,
 			current_rgb = opt.rbga || hsl2rbg(current_h, current_s, current_l),
 			current_pos = {x:172,y:22},
+			current_pos_h = 0,
+			current_pos_a = 198,
+			current_shade = 'sp_shade_primary',
 			pointer_status = 0,
 			pointer_start = "";
 
@@ -18,19 +21,19 @@ function sp(opt) {
 			module_left = new El('div').addClass('module_left').addId('sp_module_left'),
 			module_right = new El('div').addClass('module_right').addId('sp_module_right'),
 			module_bottom = new El('div').addClass('module_bottom').addId('sp_module_bottom'),
-			colorPicker = new El('div').addClass('colorPicker').addId('sp_colorPicker'),
+			colorPicker = new El('div').addClass('colorPicker pickerWrap').addId('sp_colorPicker'),
 			colorPicker_can = new El('canvas').addId('colorPicker'),
 			colorPicker_ctx = colorPicker_can.getContext('2d'),
-			huePicker = new El('div').addClass('huePicker').addId('sp_huePicker'),
+			huePicker = new El('div').addClass('huePicker pickerWrap').addId('sp_huePicker'),
 			huePicker_can = new El('canvas').addId('huePicker'),
 			huePicker_ctx = huePicker_can.getContext('2d'),
-			alphaPicker = new El('div').addClass('alphaPicker').addId('sp_alphaPicker'),
+			alphaPicker = new El('div').addClass('alphaPicker pickerWrap').addId('sp_alphaPicker'),
 			alphaPicker_can = new El('canvas').addId('alphaPicker'),
 			alphaPicker_ctx = alphaPicker_can.getContext('2d'),
 			shadesPicker = new El('ul').addClass('shadesPicker').addId('shadesPicker'),
 			shade_lighter = new El('li').addId('sp_shade_lighter'),
 			shade_light = new El('li').addId('sp_shade_light'),
-			shade_primary = new El('li').addId('ssp_hade_primary'),
+			shade_primary = new El('li').addId('sp_shade_primary').addClass('shade_selected'),
 			shade_dark = new El('li').addId('sp_shade_dark'),
 			shade_darker = new El('li').addId('sp_shade_darker'),
 			clearfix = new El('div').addClass('clearfix'),
@@ -45,12 +48,15 @@ function sp(opt) {
 			input_h = new El('input').addType('number'),
 			input_s = new El('input').addType('number'),
 			input_l = new El('input').addType('number'),
-			input_hex = new El('input').addType('text');
+			input_hex = new El('input').addType('text'),
+			knob_cp = new El('div').addClass('knob knob_cp'),
+			knob_hp = new El('div').addClass('knob knob_hp'),
+			knob_ap = new El('div').addClass('knob knob_ap');
 
 	colorPicker_can.draggable = false;
 	huePicker_can.draggable = false;
 	alphaPicker_can.draggable = false;
-		
+
 	readout_rgba.appendMany(DOM_tableRow(input_r, input_g, input_b, input_a));
 	readout_rgba.appendMany(DOM_tableHeader("r", "g", "b", "a"));
 
@@ -72,9 +78,9 @@ function sp(opt) {
 	alphaPicker_can.width = 200;
 	alphaPicker_can.height = 20;
 
-	colorPicker.appendMany(colorPicker_can);
-	huePicker.appendMany(huePicker_can);
-	alphaPicker.appendMany(alphaPicker_can);
+	colorPicker.appendMany(knob_cp, colorPicker_can);
+	huePicker.appendMany(knob_hp, huePicker_can);
+	alphaPicker.appendMany(knob_ap, alphaPicker_can);
 	module_left.appendMany(colorPicker, huePicker, alphaPicker);
 
 	module_top.appendMany(module_left, module_right, clearfix);
@@ -96,7 +102,14 @@ function sp(opt) {
 	}
 
 	function set_color_RGB(rgb){
-	  var hsl = rgba2hsl(rgb);
+	  var hsl = rgba2hsl(rgb),
+	  		hex;
+	  if(!hsl) {
+	  	hsl = [0,0,0];
+	  	hex = "#000000";
+	  } else {
+	  	hex = rgbToHex(current_rgb);
+	  }
 	  current_rgb = rgb;
 	  current_h = hsl[0];
 	  current_s = hsl[1];
@@ -111,7 +124,7 @@ function sp(opt) {
 	  input_b.value = current_rgb[2];
 	  input_a.value = current_a;
 
-	  input_hex.value = rgbToHex(current_rgb);
+	  input_hex.value = hex;
 	}
 
 	function set_color_pos(pos){
@@ -195,34 +208,33 @@ function sp(opt) {
           endS = 100,
           x = 0,
           y = 0,
-          color,
-          width = 198,
-          height = 128,
-          rate = 200,         
-          dist = (endS - startS)/width,
-          incr = (endS - startS)/width/130;
+          width = 200,
+          height = 130;;
 
-      colorPicker_ctx.clearRect(0, 0, width, 130);
+ 			var imageData = colorPicker_ctx.getImageData(0, 0, width, height);
+			var buf = new ArrayBuffer(imageData.data.length);
+			var buf8 = new Uint8ClampedArray(buf);
+			var data = new Uint32Array(buf);
 
-      for (y = 130; y>=0; y--){
-      	x = 0;
-        for (x = 200; x >= 0; x--) {        
+      for (y = 0; y < height; y++){
+        for (x = 0; x < width; x++) {
           s = Math.easeInQuad(x, startS, (100 - startS ), width);
-          l = Math.easeOutQuad(x, startL, -(startL - endL), width); 
-          colorPicker_ctx.fillStyle = "hsl(" + h + "," + s + "%," + l + "%)";
-          // if(l>25 && l<26 || l>50 && l<51 || l>75 && l<76 || l > 99 || l < 1) {
-          // 	colorPicker_ctx.fillStyle = "pink";
-          // } 
-          // if(s>25 && s < 26 || s>50 && s < 51 ||s>75 && s < 76 || s>99 || s < 1){
-          // 	colorPicker_ctx.fillStyle = "red";
-          // }
-          colorPicker_ctx.fillRect(x, y, 1, 1);             
+          l = Math.easeOutQuad(x, startL, -(startL - endL), width);
+          var rgb = hsl2rbg(h,s,l);
+        	data[y * width + x] =
+            (255    << 24) |    // alpha
+            (rgb[2] << 16) |    // blue
+            (rgb[1] <<  8) |    // green
+             rgb[0];            // red
         }
       	startS = Math.easeOutExpo(y, 100, -100, height);
-      	startL = Math.linearTween(y, 100, -100, height);
+      	startL = Math.easeInQuad(y, 100, -100, height);
       	endL = Math.easeInQuad(y, 50, -50, height);
       }
+      imageData.data.set(buf8);
+			colorPicker_ctx.putImageData(imageData, 0, 0);
     }
+
 	function draw_huePicker() {
 		var width = 198,
       	height = 18,
@@ -237,25 +249,38 @@ function sp(opt) {
   }
 
   function draw_alphaPicker() {
-    var width = 198,
-  		height = 18;
+    var width = 200,
+  			height = 20,
+  			y= 0,
+  			x = 0;
 
+  	var imageData = alphaPicker_ctx.getImageData(0, 0, width, height);
+			var buf = new ArrayBuffer(imageData.data.length);
+			var buf8 = new Uint8ClampedArray(buf);
+			var data = new Uint32Array(buf);
     alphaPicker_ctx.clearRect(0, 0, width, height);
 
     var a = 0;
-    for (var x = 0; x < width; x++) {
-      alphaPicker_ctx.fillStyle = "rgba(" + current_rgb[0] + "," + current_rgb[1] + "," + current_rgb[2] + "," + a/100 +")";
-      alphaPicker_ctx.fillRect(x, 0, 1, height);
-      a = a + (100 / width);
+    for (y = 0; y < height; y++){
+    	for (x = 0; x < width; x++) {
+        	data[y * width + x] =
+            (x/width*255   << 24) |    // alpha
+            (current_rgb[2] << 16) |    // blue
+            (current_rgb[1] <<  8) |    // green
+             current_rgb[0];            // red
+    	}
     }
+
+    imageData.data.set(buf8);
+		alphaPicker_ctx.putImageData(imageData, 0, 0);
   }
 
   function draw_shades(){
+  	set_bgc_RGBA(shade_primary, current_rgb);
   	set_bgc_RGBA(shade_lighter, shade(current_rgb, 15));
   	set_bgc_RGBA(shade_light, shade(current_rgb, 8));
-  	set_bgc_RGBA(shade_primary, current_rgb);
   	set_bgc_RGBA(shade_dark, shade(current_rgb, -8));
-  	set_bgc_RGBA(shade_darker, shade(current_rgb, -15));  	
+  	set_bgc_RGBA(shade_darker, shade(current_rgb, -15));
   }
 
 	function update_readout_display(){
@@ -274,17 +299,17 @@ function sp(opt) {
 		}
 	}
 
-	function update_hue_change(h){	
+	function update_hue_change(h){
 		draw_colorPicker(h);
 		set_color_RGB(colorPicker_ctx.getImageData(current_pos.x, current_pos.y, 1, 1).data);
 		draw_shades();
-		draw_alphaPicker();		
+		draw_alphaPicker();
 	}
 
 	function update_alpha_change(){
 		set_color_pos();
 		update_readout_display();
-		draw_shades();		
+		draw_shades();
 	}
 
 	function update_selected(pos){
@@ -296,7 +321,7 @@ function sp(opt) {
 		draw_shades();
 	}
 
-	// event listeners 
+	// event listeners
 	readout_change.addEventListener('click', function(){
 		readout++;
 		if(readout>2)readout=0;
@@ -304,45 +329,95 @@ function sp(opt) {
 	});
 
 	// log click status
-	module.addEventListener('pointerdown', function(){
-		pointer_status = 1;
+	document.addEventListener('pointerdown', function(e){
+		//do I stil need this?
 	});
 	document.addEventListener('pointerup', function(){
 		pointer_status = 0;
+		pointer_start = "";
+	});
+
+	function processInteract(e) {
+			var mod = module.getBoundingClientRect(),
+				x = e.x - mod.left,
+				y = e.y - mod.top,
+				offsetX = 0,
+				offsetY = 0;
+		if(pointer_start === "colorPicker"){
+			offsetX = offsetY = 5;
+		} else if(pointer_start === "huePicker"){
+			offsetX = 5;
+			offsetY = 140;
+		} else if(pointer_start === "alphaPicker"){
+			offsetX = 5;
+			offsetY = 165;
+		}
+
+		x -= offsetX;
+		y -= offsetY;
+
+		if(pointer_start === "colorPicker" && pointer_status === 1){
+			if(y > 128)y=128;
+			if(x > 198)x=198;
+			if(y < 0)y=0;
+			if(x < 0)x=0;
+			knob_cp.style.transform = "translate(" + x + "px," + y + "px)";
+      set_color_RGB(colorPicker_ctx.getImageData(x, y, 1, 1).data);
+      update_selected({x:x, y:y});
+    } else if(pointer_start === "huePicker" && pointer_status === 1 ){
+    	if(y > 18)y=18;
+			if(x > 198)x=198;
+			if(y < 0)y=0;
+			if(x < 0)x=0;
+			 knob_hp.style.transform = "translateX(" + x + "px)";
+			update_hue_change(x*(360/198));
+
+    } else if(pointer_start === "alphaPicker" && pointer_status === 1 && y < 18  && x < 198){
+      var temp_a = precisionRound(x/2/95,3);
+      if(temp_a > 1)temp_a=1;
+      if(temp_a < 0)temp_a=0;
+      current_a = temp_a;
+      update_alpha_change();
+    }
+	}
+
+	module.addEventListener('pointerdown', function(e){
+		pointer_status = 1;
+		pointer_start = e.target.id;
+		if(e.target.draggable) e.target.draggable = false;
+		processInteract(e);
+	})
+
+	module.addEventListener('pointermove', function(e){
+		// get pos within module
+		if(e.target.draggable) e.target.draggable = false;
+		processInteract(e);
+		// console.log(x, y, pointer_start, pointer_status)
 	});
 
 	colorPicker_can.addEventListener('pointerdown', function(e){
-		pointer_start = "color";
-    set_color_RGB(colorPicker_ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data);
-    update_selected({x:e.offsetX, y:e.offsetY});      
+		// e.target.draggable = false;
+    // set_color_RGB(colorPicker_ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data);
+    // update_selected({x:e.offsetX, y:e.offsetY});
 	});
 
 	colorPicker_can.addEventListener('pointermove', function(e){
-		e.target.draggable = false;
-      if(pointer_start === "color" && pointer_status === 1 && e.offsetY < 128  && e.offsetX < 198){
-      	set_color_RGB(colorPicker_ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data);
-      	update_selected({x:e.offsetX, y:e.offsetY});
-      }
+		// e.target.draggable = false;
 	});
 
 	huePicker_can.addEventListener('pointerdown', function(e){
-		pointer_start = "hue";
-    var rgb = huePicker_ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data;
-    var hsl = rgba2hsl(rgb);   	
-    update_hue_change(hsl[0]);
+		// e.target.draggable = false;
+    // var rgb = huePicker_ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data;
+    // var hsl = rgba2hsl(rgb);
+    // update_hue_change(hsl[0]);
 	});
 
 	huePicker_can.addEventListener('pointermove', function(e){
-		e.target.draggable = false;
-      if(pointer_start === "hue" && pointer_status === 1 && e.offsetY < 18  && e.offsetX < 198){
-      	 var rgb = huePicker_ctx.getImageData(e.offsetX, e.offsetY, 1, 1).data;
-      	var hsl = rgba2hsl(rgb);
-   			update_hue_change(hsl[0]);
-      }
+		// e.target.draggable = false;
 	});
 
 	alphaPicker_can.addEventListener('pointerdown', function(e){
-		pointer_start = "alpha";
+		// e.target.draggable = false;
 		var temp_a = precisionRound(e.offsetX/2/95,3);
       	if(temp_a > 1)temp_a=1;
       	if(temp_a < 0)temp_a=0;
@@ -351,21 +426,51 @@ function sp(opt) {
 	});
 
 	alphaPicker_can.addEventListener('pointermove', function(e){
-		e.target.draggable = false;
-      if(pointer_start === "alpha" && pointer_status === 1 && e.offsetY < 18  && e.offsetX < 198){
-      	var temp_a = precisionRound(e.offsetX/2/95,3);
-      	if(temp_a > 1)temp_a=1;
-      	if(temp_a < 0)temp_a=0;
-      	current_a = temp_a;
-      	update_alpha_change();
-      }
+		// e.target.draggable = false;
 	});
+
+	shadesPicker.addEventListener('pointerdown', function(e){
+		if(e.target.id === "sp_shade_primary"){
+			shade_primary.className = "shade_selected";
+			shade_selected = e.target.id;
+		} else {
+			shade_primary.className = "";
+		}
+
+		if(e.target.id === "sp_shade_light"){
+			shade_light.className = "shade_selected";
+			shade_selected = e.target.id;
+		} else {
+			shade_light.className = "";
+		}
+
+		if(e.target.id === "sp_shade_lighter"){
+			shade_lighter.className = "shade_selected";
+			shade_selected = e.target.id;
+		} else {
+			shade_lighter.className = "";
+		}
+
+		if(e.target.id === "sp_shade_dark"){
+			shade_dark.className = "shade_selected";
+			shade_selected = e.target.id;
+		} else {
+			shade_dark.className = "";
+		}
+
+		if(e.target.id === "sp_shade_darker"){
+			shade_darker.className = "shade_selected";
+			shade_selected = e.target.id;
+		} else {
+			shade_darker.className = "";
+		}
+	})
 
 
 
 	// init
 	function init(){
-		
+
 		update_readout_display();
 		draw_colorPicker();
 		draw_huePicker();
@@ -377,8 +482,9 @@ function sp(opt) {
 
 
 	// test output
-	document.getElementById('test1').appendMany(module);
-	console.log(module);
+	// console.log(opt.id, document.getElementById(opt.id));
+	document.getElementById(opt.id).appendMany(module);
+	// console.log(module);
 
 	init();
 	set_color_RGB([12,120,50]);
