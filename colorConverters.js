4b29655049b504a105c2863eb739aec7cf6afe4a
jirayama <jillianedward@gmail.com>
2018-05-15 16:21:48 +0900

    Add files via upload

--- colorConverters.js
+++ colorConverters.js
@@ -20,9 +20,9 @@ function hsl2rbg(h, s, l) {
   t2 = 2 * l - t1;
   //hue
   h = h / 360;
-  tR = h + 1/3;
+  tR = h + 1 / 3;
   tB = h;
-  tG = h - 1/3;
+  tG = h - 1 / 3;
 
   function convertColor(color) {
     var result;
@@ -41,7 +41,6 @@ function hsl2rbg(h, s, l) {
       result = t2;
     }
     return Math.round(result * 255);
-
   }
   r = convertColor(tR);
   g = convertColor(tB);
@@ -49,37 +48,8 @@ function hsl2rbg(h, s, l) {
   return [r, g, b, 255];
 }
 
-// function hsl2rbg(h, s, l){
-//     var r, g, b;
-//   s = s / 100;
-//   l = l / 100;
-//   h = h / 360;
-
-//     if(s == 0){
-//         r = g = b = l; // achromatic
-//     }else{
-//         function hue2rgb(p, q, t){
-//             if(t < 0) t += 1;
-//             if(t > 1) t -= 1;
-//             if(t < 1/6) return p + (q - p) * 6 * t;
-//             if(t < 1/2) return q;
-//             if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-//             return p;
-//         }
-
-//         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-//         var p = 2 * l - q;
-//         r = hue2rgb(p, q, h + 1/3);
-//         g = hue2rgb(p, q, h);
-//         b = hue2rgb(p, q, h - 1/3);
-//     }
-
-//     return [r * 255, g * 255, b * 255];
-// }
-
-
- function rgba2hslString(rgba){
- return rgba2hsl(rgba).toString();
+function rgba2hslString(rgba) {
+  return rgba2hsl(rgba).toString();
 }
 
 function rgba2hsl(rbg) {
@@ -109,7 +79,7 @@ function rgba2hsl(rbg) {
   h = h * 60;
   if (h < 0) {
     h = 0;
-  } else if(h>360){
+  } else if (h > 360) {
     h = 360;
   }
   h = Math.round(h);
@@ -121,40 +91,10 @@ function rgba2hsl(rbg) {
   return [h, s, l];
 }
 
-// function rgba2hsl(rgb) {
-//   var r = rgb[0]/= 255, 
-//       g = rgb[1]/= 255, 
-//       b = rgb[2]/= 255;
-
-//   var max = Math.max(r, g, b), min = Math.min(r, g, b);
-//   var h, s, l = (max + min) / 2;
-
-//   if (max == min) {
-//     h = s = 0; // achromatic
-//   } else {
-//     var d = max - min;
-//     s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-
-//     switch (max) {
-//       case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-//       case g: h = (b - r) / d + 2; break;
-//       case b: h = (r - g) / d + 4; break;
-//     }
-
-//     h /= 6;
-//   }
-
-//   h = Math.round(h*360);
-//   s = Math.round(s * 100);
-//   l = Math.round(l * 100);
-
-//   return [ h, s, l ];
-// }
-
 function rgbToHex(rgb) {
-    function c(v) {
-        var hex = v.toString(16);
-        return hex.length === 1 ? "0" + hex : hex;
-    }
-    return "#" + c(rgb[0]) + c(rgb[1]) + c( rgb[2]);
+  function c(v) {
+    var hex = v.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+  }
+  return "#" + c(rgb[0]) + c(rgb[1]) + c(rgb[2]);
 }
